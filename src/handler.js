287d228dc02ef93ab07b91efd52a12df0a9d6a1d
Mas 'Edi <aslekmak54@gmail.com>
2022-04-13 13:26:07 +0700

    Again changes

--- src/handler.js
+++ src/handler.js
@@ -13,6 +13,28 @@ const addBookHandler = (request, h) => {
     reading
   } = request.payload
 
+  const id = nanoid(16)
+  const finished = (pageCount === readPage)
+  const insertedAt = new Date().toISOString()
+  const updatedAt = insertedAt
+
+  const newBook = {
+    id,
+    name,
+    year,
+    author,
+    summary,
+    publisher,
+    pageCount,
+    readPage,
+    finished,
+    reading,
+    insertedAt,
+    updatedAt
+  }
+
+  books.push(newBook)
+
   if (name === undefined) {
     const response = h.response({
       status: 'fail',
@@ -30,27 +52,6 @@ const addBookHandler = (request, h) => {
     response.code(400)
     return response
   }
-
-  const id = nanoid(16)
-  const finished = (pageCount === readPage)
-  const insertedAt = new Date().toISOString()
-  const updatedAt = insertedAt
-  const newBook = {
-    id,
-    name,
-    year,
-    author,
-    summary,
-    publisher,
-    pageCount,
-    readPage,
-    finished,
-    reading,
-    insertedAt,
-    updatedAt
-  }
-
-  books.push(newBook)
 
   const isSucces = books.filter((book) => book.id === id).length > 0
 
@@ -107,8 +108,8 @@ const getAllBooksHandler = (request, h) => {
 }
 
 const getBookByIdHandler = (request, h) => {
-  const { bookId } = request.params
-  const book = books.filter((book) => book.id === bookId)[0]
+  const { id } = request.params
+  const book = books.filter((book) => book.id === id)[0]
 
   if (book !== undefined) {
     return {
@@ -128,7 +129,7 @@ const getBookByIdHandler = (request, h) => {
 }
 
 const editBookByIdHandler = (request, h) => {
-  const { bookId } = request.params
+  const { id } = request.params
   const {
     name,
     year,
@@ -141,7 +142,7 @@ const editBookByIdHandler = (request, h) => {
   } = request.payload
 
   const updatedAt = new Date().toISOString()
-  const index = books.findIndex((book) => book.id === bookId)
+  const index = books.findIndex((book) => book.id === id)
 
   if (index !== -1) {
     if (name === undefined) {
@@ -199,9 +200,9 @@ const editBookByIdHandler = (request, h) => {
 }
 
 const deleteBookByIdHandler = (request, h) => {
-  const { bookId } = request.params
+  const { id } = request.params
 
-  const index = books.findIndex((book) => book.id === bookId)
+  const index = books.findIndex((book) => book.id === id)
 
   if (index !== -1) {
     books.splice(index, 1)
